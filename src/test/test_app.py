749a7df0d299b9190b1cfbb0d9f07c7c06b64cf4
richardlau <rilau@thoughtworks.com>
2020-10-15 11:29:30 -0400

    [Rich] - Fix docker-compose for testing endpoint

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -22,3 +22,4 @@ def test_dockerfile():
     response = requests.get("http://127.0.0.1:5000/")
 
     assert response.status_code == 200
+    assert response.content.decode('utf-8') == "Hello, World!"
