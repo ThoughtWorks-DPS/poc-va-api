79263da7231e96aedc50ea27883d22db431c595d
Punit Lad <plad@thoughtworks.com>
2020-10-21 15:07:30 -0400

    [Seun | Punit] - move endpoint documentation to file

--- src/app.py
+++ src/app.py
@@ -1,21 +1,12 @@
 from flask import Flask
 from flask import Flask, jsonify
 import os
-from flasgger import Swagger
+from flasgger import Swagger, swag_from
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
 @app.route('/')
+@swag_from('static/hello_world.yml')
 def hello_world():
-    """Endpoint returning hello world
-        ---
-        summary: Returns 'Hello World!'
-        responses:
-          200:
-            description: OK
-            schema:
-              id: hello_world
-              type: string
-        """
     return 'Hello, World!'
