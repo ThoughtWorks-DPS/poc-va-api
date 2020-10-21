58f5220d6546e27ce5139717b3f5c6645fddf08a
Punit Lad <plad@thoughtworks.com>
2020-10-21 16:03:03 -0400

    [Seun | Punit] - add test for documentation. currently skipped

--- test/unit/test_app.py
+++ test/unit/test_app.py
@@ -1,4 +1,5 @@
 import pytest
+import yaml
 
 from src import app
 
@@ -15,3 +16,16 @@ def test_hello_world(client):
     response = client.get('/')
     assert response.data.decode('utf-8') == 'Hello, World!'
     assert response.status == '200 OK'
+
+# def test_hello_world_documentation(client):
+#     response = client.get('/')
+#
+#     with open(r'/Users/punitlad/projects/va/dps/di/poc/poc-va-api/src/static/hello_world.yml') as file:
+#         documentation = yaml.load(file)
+#         assert documentation['responses'][200] == # get the key for 200 check status
+#         print(documentation)
+# #         doc_response = documentation['responses']['200']
+# #         println(doc_response)
+#
+# #     assert response.status == doc_response
+
